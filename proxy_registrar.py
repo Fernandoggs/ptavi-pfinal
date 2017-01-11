52a31b682358d10ad08706201639f4f8a25d74ef
Fernandoggs <fernando.ggs@gmail.com>
2017-01-11 15:04:51 +0100

    Error al recibir request del cliente -- must be str, not list

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -8,37 +8,60 @@ import socketserver
 import sys
 import json
 import time
-import uaclient
+import random
+from xml.sax import make_parser
+from xml.sax import ContentHandler
 
+class Proxy_Constructor(ContentHandler):
+
+    def __init__(self):
+
+        self.tags = []
+        self.dicc = {'server': ['name', 'ip', 'port'],
+                     'database': ['path', 'passwdpath'],
+                     'log': ['path']}
+
+    def startElement(self, name, attrs):
+
+        if name in self.dicc:
+            dicc_aux = {}
+            for atribute in self.dicc[name]:
+                dicc_aux[atribute] = attrs.get(atribute, "")
+            self.tags.append([name, dicc_aux])
+
+    def get_tags(self):
+        return self.tags
 #
 #Condiciones de entrada
 #
-if not len(sys.argv) != 2:
+if not len(sys.argv) != 1:
 	sys.exit("Usage: python uaserver.py config")
 #
 #Extracción de lo introducido por la linea de comandos
 #
 #Fichero de configuración del UA
-CONFIG = sys.argv
+CONFIG = sys.argv[1]
 #
 #Inicio del manejador
 #
 #Manejo del fichero de configuración
 parser = make_parser()
-server_handler = uaclient.UA_Constructor()
-parser.setContentHandler(server_handler)
+proxy_handler = Proxy_Constructor()
+parser.setContentHandler(proxy_handler)
 parser.parse(open(CONFIG))
-info = server_handler.get_tags()
+info = proxy_handler.get_tags()
 #######TRAZA#######
 print(info)
 #######TRAZA#######
 
+#Extrae la informacion del fichero de configuración
+server_name = info[0][1]['name']
 server_ip = info[0][1]['ip']
 server_port = info[0][1]['port']
-clients_fich = info[1][1]['path']
+registered_fich = info[1][1]['path']
 passwd_fich = info[1][1]['passwdpath']
 log_fich = info[2][1]['path']
-nonce = '4815162342'
+nonce = random.randint(0,99999999999999999)
 
 #Usuarios registrados
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
@@ -48,19 +71,58 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     users_dicc: {}
     #Abro el fichero de registro de clientes en modo escritura
     def register2json(self):
-        json.dump(self.users_dicc, open(clients_fich,'w'))
+        json.dump(self.users_dicc, open(registered_fich,'w'))
 
     #Compruebo si existe el fichero y lo leo
-    def json2register(self):
+    def json2registered(self):
+        try:
+            with open(registered_fich) as registered_file:
+                self.users_dicc = json.load(registered_file)
+        except:
+            self.register2json()
+
+    #Borra usuarios del registro de usuarios registrados
+    def delete(self):
+        aux_list = []
+        for user in self.users_dicc:
+            expires = int(self.users_dicc[user][-1])
+            if expires < time.time():
+                aux_list.append(user)
+        for client in aux_list:
+            del self.users_dicc[client]
+            print('Deleting: ',client)
+
+
+    #Registro de nuevos Usuarios
+    def register(self,client_expire,register_user,client_address,client_port):
+        self.json2registered()
+        self.time = float(time.time())
+        self.time_expire = float(time.time()) + float(client_expire)
+        self.client_info=[client_address,client_port,self.time,self.time_expire]
+        self.users_dicc[register_user] = self.client_info
+        if int(register_time) == 0:
+                del self.users_dicc[register_user]
+        self.wfile.write(b"SIP/2.0 200 0K\r\n")
+        self.delete()
+        self.register2json
+
+    def handle(self):
+        while 1:
+            line = self.rfile.read()
+            if not line:
+                break
+            request = line.decode('utf-8').split()
+            print("El cliente nos manda: " + request)
+            ###HACER LOG
+
+
 
 
 if __name__ == "__main__":
-	#Crea un servidor UDP.Hay q pasar 2 parametros: ip y puerto donde voy a escuchar /
-	# y la clase
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
-    print("Lanzando servidor UDP de eco...")
+    serv = socketserver.UDPServer(('', int(server_port)), SIPRegisterHandler)
+    print("Server " + server_name + " listening at port: " + server_port + "...")
     #Se crea esta excepción para q al salir del servidor con crtl+c salga el mensaje de Finalizado
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
-        print("Finalizado servidor")
+        print("----SERVER FINISHED----")
