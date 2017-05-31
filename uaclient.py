723a51b7b8c811bd287e54558113b3ba40f18125
Fernandoggs <fernando.ggs@gmail.com>
2017-05-31 19:52:49 +0200

    Implementado do_log y delete_log

--- uaclient.py
+++ uaclient.py
@@ -34,6 +34,7 @@ class UA_Constructor(ContentHandler):
 
     def get_tags(self):
         return self.tags
+
 #
 #Condiciones de entrada
 #
@@ -69,51 +70,63 @@ proxy_ip = info[3][1]['ip']
 proxy_port = info[3][1]['port']
 log_path = info[4][1]['path']
 audio_path = info[5][1]['path']
+
+#Procedimiento que genera una nueva entrada de log
+def do_log(entry):
+
+    log = open(log_path, 'a+')
+    log.write('\r\n' + time.strftime('%Y%m%d%H%M%S ', time.gmtime(time.time())) + entry)
+    log.close()
+
+#Procedimiento que borra el fichero de log
+def delete_log():
+    log= open(log_path, 'w')
+    log.close()
+
 #Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((proxy_ip, int(proxy_port)))
 
-######
-# FALTA AÑADIR EL 'Starting...' AL FICHERO LOG
-######
 try:
     #Actuaciones en función del método especificado por el cliente
     if METHOD == "REGISTER":
 	    #Estructura de mensaje REGISTER
-	    Request = 'REGISTER sip:' +username + ':' + server_port + ' SIP/2.0\r\n'
-	    Request += 'Expires:' + OPTION + '\r\n'
+	    request = 'REGISTER sip:' +username + ':' + server_port + ' SIP/2.0\r\n'
+	    request += 'Expires:' + OPTION + '\r\n'
 
     elif METHOD == "INVITE":
 	    #Estructura de mensaje INVITE
-	    Request = 'INVITE sip:' + OPTION + ' SIP/2.0\r\n'
-	    Request += 'Content-Type: application/sdp\r\n\r\n'
-	    Request += 'v=0\r\n'
-	    Request += 'o=' + username + ' ' + server_ip + '\r\n'
-	    Request += 's=mysession\r\n'
-	    Request += 't=0\r\n'
-	    Request += 'm=audio ' + rtp_port + ' RTP\r\n'
+	    request = 'INVITE sip:' + OPTION + ' SIP/2.0\r\n'
+	    request += 'Content-Type: application/sdp\r\n\r\n'
+	    request += 'v=0\r\n'
+	    request += 'o=' + username + ' ' + server_ip + '\r\n'
+	    request += 's=mysession\r\n'
+	    request += 't=0\r\n'
+	    request += 'm=audio ' + rtp_port + ' RTP\r\n'
 
     elif METHOD == "BYE":
 	    #Estructura de mensaje BYE
-	    Request = 'BYE sip:'+ OPTION + ' SIP/2.0\r\n'
+	    request = 'BYE sip:'+ OPTION + ' SIP/2.0\r\n'
     else:
-	    Request = METHOD + ' sip:'+ OPTION + ' SIP/2.0\r\n'
-        #print("The method entered is wrong, it must be REGISTER, INVITE or BYE")
-        #sys.exit("(Mind the capital letters)")
+	    request = METHOD + ' sip:'+ OPTION + ' SIP/2.0\r\n'
 
-
-    ####PASAR PRINT AL LOG
-    print("Enviando: " , Request)
-    ####PASAR PRINT AL LOG
+    delete_log()
+    entry = "Starting client: "+username
+    do_log(entry)
+    entry = "Socket connected to Proxy "+proxy_ip+":"+str(proxy_port)
+    do_log(entry)
     #Envio Request al Proxy
-    my_socket.send(bytes(Request, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(request, 'utf-8') + b'\r\n')
+    log_entry ="Sending to "+proxy_ip+':'+str(proxy_port) +' '+request.replace('\r\n',' ')
+    do_log(log_entry)
+    print("Sendin to proxy-- " , request)
     #Recibo contestacion
     data = my_socket.recv(1024)
     reply = data.decode('utf-8')
-    ####PASAR PRINT AL LOG
-    print('Recibido -- ', reply)
-    ####PASAR PRINT AL LOG
+    log_entry ="Received from "+proxy_ip+':'+str(proxy_port) +' '+reply.replace('\r\n',' ')
+    do_log(log_entry)
+    print('Received -- ', reply)
     code = reply.split(' ')[1].split(' ')[0]
     #print('Code--> ' + code)
     #if reply.startswith('SIP/2.0 401 Unauthorized'):
@@ -149,7 +162,8 @@ try:
             aEjecutar += " -p " + rtp_port + " < " + audio_path
             print("Vamos a ejecutar", aEjecutar)
             os.system(aEjecutar)
-
+    entry = "Finishing socket."
+    do_log(entry)
     print("Finishing socket...")
 
 except ConnectionRefusedError:
